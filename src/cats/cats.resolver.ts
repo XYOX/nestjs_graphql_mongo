37cd05e9cc1989bf3cda7f4ea518a077b54eba14
Christian Xyox <warsperu@gmail.com>
2021-09-01 14:20:42 -0500

    agregando graphql service

--- src/cats/cats.resolver.ts
+++ src/cats/cats.resolver.ts
@@ -1,15 +1,25 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CatsService } from './cats.service';
+import { CatType } from './dto/create-cat.dto';
+import { CatInput } from './inputs/cat.input';
 
 @Resolver()
 export class CatsResolver {
-  // constructor(
-  //   private authorsService: AuthorsService,
-  //   private postsService: PostsService,
-  // ) {}
+  constructor(private readonly catsService: CatsService) {}
 
   @Query(() => String)
   async hello() {
     return 'hola';
+  }
+
+  @Query(() => [CatType])
+  async cats() {
+    return this.catsService.findAll();
+  }
+
+  @Mutation(() => CatType)
+  async createCat(@Args('input') input: CatInput) {
+    return this.catsService.create(input);
   }
 
   // @ResolveField()
